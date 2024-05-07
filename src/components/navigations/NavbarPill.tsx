1fff99ce8d7a81170a89b264e0fe6e2826624692
obertcoy <laisobert2@gmail.com>
2024-05-07 14:11:38 +0700

    Expanding navbar on scroll

--- src/components/navigations/NavbarPill.tsx
+++ src/components/navigations/NavbarPill.tsx
@@ -1,18 +1,39 @@
+"use client"
+
 import Regenera from "../vector-graphics/Regenera";
 import Link from "next/link";
 import InputGroup from "../forms/InputGroup";
 import { Search } from "lucide-react";
+import { useEffect, useState } from "react";
 
 export default function NavbarPill() {
+
+  const [isExpanded, setIsExpanded] = useState(false)
+
+  useEffect(() => {
+    
+    const handleScroll = () => {
+      const scrollTop = window.scrollY
+  
+      setIsExpanded(scrollTop > 0)
+    }
+
+    window.addEventListener('scroll', handleScroll)
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  })
+
   return (
     <header
       className={
-        "z-50 fixed top-4 w-full flex justify-center items-center select-none"
+        `z-50 fixed w-full flex justify-center items-center select-none transition-all duration-200  ${!isExpanded ? 'top-4' : 'top-0 bg-light-background-100'}`
       }
     >
-      <nav
+      <nav 
         className={
-          "max-w-[67rem] w-full py-3 px-8 flex items-center justify-between bg-light-background-100 shadow rounded-full"
+          `max-w-[67rem] w-full py-3 px-8 flex items-center justify-between ${!isExpanded ? 'bg-light-background-100 shadow rounded-full' : '' }`
         }
       >
         <div className={"w-[16rem] flex items-center justify-start gap-x-4"}>
