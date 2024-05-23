f9060474ac4770f5e1778dac72ea7d79567c8146
6ixB <124480418+6ixB@users.noreply.github.com>
2024-05-23 10:12:25 +0700

    fix: refactored layout providers

---
+++ src/components/Providers.tsx
@@ -0,0 +1,17 @@
+import ReactQueryProvider from "@/components/react-query/ReactQueryProvider";
+import { SessionProvider } from "next-auth/react";
+import { auth } from "@/lib/next-auth/auth";
+
+interface ProvidersProps {
+  children: React.ReactNode;
+}
+
+export default async function Providers({ children }: ProvidersProps) {
+  const session = await auth();
+
+  return (
+    <SessionProvider session={session}>
+      <ReactQueryProvider>{children}</ReactQueryProvider>
+    </SessionProvider>
+  );
+}
