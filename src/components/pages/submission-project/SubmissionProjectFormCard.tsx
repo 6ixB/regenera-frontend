056c1b4640b5acb7caa135f2a972910d72b84236
obertcoy <laisobert2@gmail.com>
2024-06-12 12:01:19 +0700

    feat: submission image fixed

--- src/components/pages/submission-project/SubmissionProjectFormCard.tsx
+++ src/components/pages/submission-project/SubmissionProjectFormCard.tsx
@@ -1,25 +1,46 @@
 import { ProjectEntity, ProjectObjectiveEntity } from "@/lib/model/project/project.entity";
 import { Camera, Plus, PlusCircle, ScanEye } from "lucide-react";
 import Image from "next/image";
+import { useRef, useState } from "react";
 
 interface SubmissionProjectFormCardProps {
     objective: ProjectObjectiveEntity,
-    idx: number
+    idx: number,
+    onFileChange: (idx: number, file: File) => void
+
 }
 
-export default function SubmissionProjectFormCard({ objective, idx }: SubmissionProjectFormCardProps) {
+export default function SubmissionProjectFormCard({ objective, idx, onFileChange }: SubmissionProjectFormCardProps) {
+
+    const [imagePreview, setImagePreview] = useState(objective?.submission?.imageUrl || "");
+    const fileInputRef = useRef<HTMLInputElement>(null);
+
+    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const file = event.target.files?.[0];
+        if (file) {
+            setImagePreview(URL.createObjectURL(file))
+            onFileChange(idx, file)
+            
+        }
+    };
+
+    const handleImageClick = () => {
+        fileInputRef.current?.click();
+    };
+
+
     return (
         <div className="w-full h-fit flex flex-col border rounded-lg border-light-background-300 shadow bg-light-background-100 ">
 
-            <div className="w-full flex flex-col hover:cursor-pointer">
-                {objective?.submission?.imageUrl ?
+            <div className="w-full flex flex-col hover:cursor-pointer" onClick={handleImageClick}>
+                {imagePreview ?
                     <Image
                         width={0}
                         height={0}
                         sizes={"100vw"}
                         className={`w-full h-auto max-h-64 aspect-square object-cover border rounded-t-lg`}
                         src={
-                            objective?.submission?.imageUrl || ""
+                            imagePreview
                         }
                         alt={"User Profile"} />
                     :
@@ -30,6 +51,13 @@ export default function SubmissionProjectFormCard({ objective, idx }: Submission
 
                 <p className="text-lg text-light-text-100 font-medium">{objective.objective}</p>
             </div>
+            <input
+                type="file"
+                ref={fileInputRef}
+                accept="image/*"
+                onChange={handleFileChange}
+                className="hidden"
+            />
         </div>
     )
 }
